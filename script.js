f957cb87a29cd370255e76a381731c3f7a3d1726
Somaditya Tripathi FD <somaditya.tripathi@swiftrut.com>
2024-11-28 05:48:36 +0530

    book page ui change

---
+++ script.js
@@ -0,0 +1,16 @@
+document.getElementById("menu-toggle").addEventListener("click", function () {
+    const mobileMenu = document.getElementById("mobile-menu");
+    mobileMenu.classList.toggle("hidden");
+  });
+  
+  document.getElementById("close-menu").addEventListener("click", function () {
+    const mobileMenu = document.getElementById("mobile-menu");
+    mobileMenu.classList.add("hidden");
+  });
+  
+  const mobileLinks = document.querySelectorAll("#mobile-menu a");
+  mobileLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+      document.getElementById("mobile-menu").classList.add("hidden");
+    });
+  });
