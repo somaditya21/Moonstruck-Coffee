1ff5307e4407ff73603e26833a4d1657e3168f16
Somaditya Tripathi FD <somaditya.tripathi@swiftrut.com>
2024-11-28 10:36:42 +0530

    Book Section

--- script.js
+++ script.js
@@ -18,29 +18,3 @@ document.getElementById("menu-toggle").addEventListener("click", function () {
 
 
 
-tailwind.config = {
-        theme: {
-          extend: {
-            container: {
-              padding: {
-                DEFAULT: "1rem",
-                sm: "2rem",
-                lg: "4rem",
-                xl: "5rem",
-                "2xl": "6rem",
-              },
-            },
-            fontFamily: {
-              sans: ['"Josefin Sans"', "sans-serif"],
-              josefin: ['"Josefin Sans"', "sans-serif"],
-              antonio: ['"Antonio Fischer"', "cursive"],
-              italiana: ['"Italiana"', "serif"],
-            },
-            backgroundImage: {
-              "gradient-border":
-                "linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, #1D71B8 100%)",
-            },
-          },
-        },
- };
-   
