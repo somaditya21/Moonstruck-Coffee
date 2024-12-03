c80c09adc777567573eb15594d3098d390dc3675
Somaditya Tripathi FD <somaditya.tripathi@swiftrut.com>
2024-12-04 02:13:04 +0530

    update some pages

--- script.js
+++ script.js
@@ -17,3 +17,27 @@ document.getElementById("menu-toggle").addEventListener("click", function () {
 
 
 
+
+  // Get the current page URL
+  const currentPage = window.location.pathname.split("/").pop();
+
+  // Get all menu links
+  const menuLinks = document.querySelectorAll("#menu a");
+
+  // Add 'active' class to the matching link
+  menuLinks.forEach(link => {
+    if (link.getAttribute("href") === currentPage) {
+      link.classList.add("text-white", "font-bold"); // Add active styles
+    } else {
+      link.classList.remove("text-white", "font-bold"); // Remove active styles from others
+    }
+  });
+
+  // Date picker
+
+  
+
+
+
+
+
